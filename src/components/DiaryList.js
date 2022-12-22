f91738957ceb6d511005aaf6a4eb30c3f2428f7c
jamie7dev <jamie7dev@gmail.com>
2022-12-22 15:27:09 +0900

    Design(HOME): "일기 리스트"

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
+import DiaryItem from "./DiaryItem";
 
 const sortOptionList = [
   { value: "latest", name: "최신순" },
@@ -87,10 +88,7 @@ const DiaryList = ({ diaryList }) => {
       </div>
 
       {getProcessDiaryList().map((it) => (
-        <div key={it.id}>
-          {it.content}
-          {it.emotion}
-        </div>
+        <DiaryItem key={it.id} {...it} />
       ))}
     </div>
   );

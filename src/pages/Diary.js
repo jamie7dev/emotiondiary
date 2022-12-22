01efb9438015c669dc369b2badb166281d413054
jamie7dev <jamie7dev@gmail.com>
2022-12-22 17:55:23 +0900

    Feat: "추가 일기 상세페이지 구현 중"

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,13 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
 
 const Diary = () => {
   const { id } = useParams();
-  console.log(id);
-  return (
-    <div>
-      <h2>Diary</h2>
-      <p>일기 상세 페이지</p>
-    </div>
-  );
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      if (targetDiary) {
+        //일기가 존재할 때
+        setData(targetDiary);
+      } else {
+        //일기가 없을 때
+        alert("없는 일기입니다.");
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  } else {
+    return <div className="DiaryPage"></div>;
+  }
+
+  return <div></div>;
 };
 export default Diary;
